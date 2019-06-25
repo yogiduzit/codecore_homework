fc136ddb376364cc3fec418c51b5bfa19d12be8f
yogiduzit <yverma2@my.bcit.ca>
2019-06-25 11:44:24 -0700

    Modified knexfile to development

--- notes/Knex/lab/knexfile.js
+++ notes/Knex/lab/knexfile.js
@@ -1,19 +1,15 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'pg',
     connection: {
-      database: 'knex_labs'
+      database: 'knexpress_labs'
     },
     migrations: {
       tableName: 'migrations', // Contains a history of all the migrations that have been performed.
       directory: './db/migrations'
     },
-    seeds: {
-      directory: './db/seeds'
-    }
   },
 
 };
