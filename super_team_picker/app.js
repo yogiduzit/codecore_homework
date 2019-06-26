fd636a2701394db13f722100e1783ff8a79ac9c8
yogiduzit <yverma2@my.bcit.ca>
2019-06-26 16:38:18 -0700

    Added cohort router

--- super_team_picker/app.js
+++ super_team_picker/app.js
@@ -7,7 +7,7 @@ const cohortRouter = require('./routes/cohort');
 
 const app = express();
 
-app.use('/cohort', cohortRouter);
+app.use('/cohorts', cohortRouter);
 
 // Logs all the request to console
 app.use(logger("dev"));
@@ -15,8 +15,9 @@ app.use(logger("dev"));
 // Allows arrays, JSON objects to be used in request bodies if true
 app.use(express.urlencoded({extended: true}));
 
+app.use(express.static("assets"));
 
-app.set("views", "views")
+app.set('views', "views")
 app.set("view engine", "ejs");
 
 const PORT = 3000;
