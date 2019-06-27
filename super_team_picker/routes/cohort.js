ce6a94838539c2eb3870a33c4717f2ad0f2b948e
yogiduzit <yverma2@my.bcit.ca>
2019-06-26 17:15:16 -0700

    Can insert data in database

--- super_team_picker/routes/cohort.js
+++ super_team_picker/routes/cohort.js
@@ -10,7 +10,31 @@ router.get('/new', (req, res) => {
 });
 
 router.post('/new', (req, res) => {
-  res.redirect('/cohorts');
+  knex
+  .insert({
+    imgURL: req.body.imgURL,
+    name: req.body.cohortName
+  })
+  .into('cohorts')
+  .returning('*')
+  .then(([cohort]) => {
+    for (let member of req.body.members.split(', ')) {
+      knex
+      .insert({
+        name: member,
+        cohort_name: req.body.cohortName
+      })
+      .into('members')
+      .returning('*')
+      .then(([member]) => {
+        
+      });
+    }
+    res.redirect('/cohorts');
+  });
+
+
+  
 });
 router.get('/', (req, res) => {
   res.send('<div>Cohorts</div>')
