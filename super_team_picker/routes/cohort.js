1d4b0d5a29be27c7b0d3f15fbf93bb66dd36554c
yogiduzit <yverma2@my.bcit.ca>
2019-06-27 17:53:03 -0700

    Can choose option and display type of option

--- super_team_picker/routes/cohort.js
+++ super_team_picker/routes/cohort.js
@@ -2,8 +2,13 @@ const express = require('express');
 const knex = require('../db/client');
 
 const router = express.Router();
+const cookieParser = require('cookie-parser');
 
-router.use(express.urlencoded({extended: true}));
+router.use(cookieParser());
+
+router.use(express.urlencoded({
+  extended: true
+}));
 
 // Get /new - Create a new cohort page
 router.get('/new', (req, res) => {
@@ -13,81 +18,96 @@ router.get('/new', (req, res) => {
 // POST /new - Post the new cohort data
 router.post('/new', (req, res) => {
   knex
-  .insert({
-    imgURL: req.body.imgURL,
-    name: req.body.cohortName
-  })
-  .into('cohorts')
-  .returning('*')
-  .then(([cohort]) => {
-    for (let member of req.body.members.split(', ')) {
-      knex
-      .insert({
-        name: member,
-        cohort_name: req.body.cohortName
-      })
-      .into('members')
-      .returning('*')
-      .then(([member]) => {
-        
-      });
-    }
-    res.redirect('/cohorts');
-  });
+    .insert({
+      imgURL: req.body.imgURL,
+      name: req.body.cohortName
+    })
+    .into('cohorts')
+    .returning('*')
+    .then(([cohort]) => {
+      for (let member of req.body.members.split(', ')) {
+        knex
+          .insert({
+            name: member,
+            cohort_name: req.body.cohortName
+          })
+          .into('members')
+          .returning('*')
+          .then(([member]) => {
+
+          });
+      }
+      res.redirect('/cohorts');
+    });
 });
 
 //
 router.get('/', (req, res) => {
   knex
-  .select('cohort_name', 'members.name', "imgURL")
-  .from('members')
-  .innerJoin('cohorts', 'members.cohort_name', 'cohorts.name')
-  .then(allMembersWithCohorts => {
-    const orderedCohorts = {};
+    .select('cohort_name', 'members.name', "imgURL")
+    .from('members')
+    .innerJoin('cohorts', 'members.cohort_name', 'cohorts.name')
+    .then(allMembersWithCohorts => {
+      const orderedCohorts = {};
 
-    let i = 0;
-    for (let member of allMembersWithCohorts) {
-      if (!orderedCohorts[member.cohort_name]) {
-        orderedCohorts[member.cohort_name] = {members: [member.name], imgURL: member.imgURL};
-      } else {
-        orderedCohorts[member.cohort_name]["members"].push(member.name);
+      let i = 0;
+      for (let member of allMembersWithCohorts) {
+        if (!orderedCohorts[member.cohort_name]) {
+          orderedCohorts[member.cohort_name] = {
+            members: [member.name],
+            imgURL: member.imgURL
+          };
+        } else {
+          orderedCohorts[member.cohort_name]["members"].push(member.name);
+        }
+
+        if (i == allMembersWithCohorts.length - 1) {
+          
+          res.render('pages/cohorts', {
+            orderedCohorts
+          });
+        }
+        i += 1;
       }
-      
-      if (i == allMembersWithCohorts.length - 1) {
-        res.render('pages/cohorts', {orderedCohorts});
-      }
-      i += 1; 
-    }
-    
-  });
+
+    });
 });
 
 router.get('/:id', (req, res) => {
   knex
-  .select('cohort_name', 'members.name', "imgURL")
-  .from('members')
-  .where('cohorts.id', req.params.id)
-  .innerJoin('cohorts', 'members.cohort_name', 'cohorts.name')
-  .then(cohortMembers => {
-    const cohort = {
-      members: []
-    };
-    let i = 0;
-    for (let member of cohortMembers) {
-      cohort["members"].push(member.name);
-      
-      if (i == cohortMembers.length - 1) {
-        cohort["imgURL"] = member.imgURL;
-        cohort["cohortName"] = member.cohort_name;
+    .select('cohort_name', 'members.name', "imgURL")
+    .from('members')
+    .where('cohorts.id', req.params.id)
+    .innerJoin('cohorts', 'members.cohort_name', 'cohorts.name')
+    .then(cohortMembers => {
+      const cohort = {
+        members: []
+      };
+      let i = 0;
+      for (let member of cohortMembers) {
+        cohort["members"].push(member.name);
 
-        res.send("<div>Hi</div>");
-        
+        if (i == cohortMembers.length - 1) {
+          cohort["cohortName"] = member.cohort_name;
+          cohort["id"] = req.params.id;
+
+          
+          res.render('pages/cohort', {
+            cohort, request: JSON.parse(req.cookies.request)
+          });
+
+        }
+        i += 1;
       }
-      i += 1;
-    }
+    });
+});
 
-    
-  });
-})
+router.post('/:id', (req, res) => {
+  if (req.cookies.request) {
+    res.clearCookie("request");
+  }
 
+  res.cookie("request", JSON.stringify(req.body));
+  res.redirect('/cohorts/' + req.params.id);
+});
 module.exports = router;
