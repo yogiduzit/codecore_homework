7312e86d54ee316047b74d3a9d1671fc352b6118
yogiduzit <yverma2@my.bcit.ca>
2019-06-28 13:56:02 -0700

    Added logic to create message

--- notes/Rails Basics/awesome_answers/config/routes.rb
+++ notes/Rails Basics/awesome_answers/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # This defines a route that says on a GET request to the
+  # root domain, run the index action of WelcomeController.
+  get('/', {to: 'welcome#index', as:'root'});
+  get '/contacts/new', {to: 'contacts#new' };
+  post '/contacts', {to: 'contacts#create'}
+  # get('/contacts/create', {to: 'contacts/'})
 end
