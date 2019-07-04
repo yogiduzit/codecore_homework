58b2fd59a4a7f4f66bc383eab9b8633e9da4da94
yogiduzit <yverma2@my.bcit.ca>
2019-07-04 13:23:21 -0700

    Added association question -> answers

--- awesome_answers/app/models/question.rb
+++ awesome_answers/app/models/question.rb
@@ -1,11 +1,23 @@
 class Question < ApplicationRecord
-  # To check if a question has a unique title.
+
+  # Adds a one to many relation from question to answers
+  # If one question is deleted, all the answers related to it 
+  # get deleted as well. If we use dependent: nullify
+  # it will change the id's of the answers related to the deleted
+  # question to NULL.
+  has_many(:answers, dependent: :destroy)
+
+  # To check if a question is present and has a unique title.
   validates(:title, presence:true, uniqueness: true);
+
+  # To check if body is present
   validates(:body, presence: {message: "must exist"});
 
   # Custom validation
   # Use singular method names for custom validations.
   validate :no_monkey
+
+
   before_validation(:set_default_like_count)
 
   # Creating a scope with a class method
@@ -25,12 +37,13 @@ class Question < ApplicationRecord
 
   private
   def no_monkey
+
     # & ensures that if the called method does not
     # exist for the particular object. It just returns 'nil'
     # instead of an error.
     if body&.downcase&.include?("monkey")
-      # adding an error makes a record invalid
 
+      # adding an error makes a record invalid
       self.errors.add(:body, "Must not have monkeys")
     end
 
@@ -40,4 +53,5 @@ class Question < ApplicationRecord
     self.like_count ||= 0
   end
 
+  
 end
