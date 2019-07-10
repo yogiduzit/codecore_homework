ae5bbf4aaf564003ecee307dacb54b7dd8c120fe
yogiduzit <yverma2@my.bcit.ca>
2019-07-09 17:35:58 -0700

    Added seed file

--- homework/blog_on_rails/db/seeds.rb
+++ homework/blog_on_rails/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+LOREM_IPSUM = "Lorem Ipsum is simply dummy text of the printing and typesetting industry
+. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+
+NUM_POSTS = 50
+
+NUM_POSTS.times do
+  post = Post.new(
+    title: Faker::Name.unique.name,
+    body: LOREM_IPSUM 
+  )
+  post.save if post.valid?
+end
