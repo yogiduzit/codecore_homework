83e02124726b860d88db7507f0876c4b8a8d4f42
yogiduzit <yverma2@my.bcit.ca>
2019-07-10 18:19:18 -0700

    Logic to show, create and new posts

--- homework/blog_on_rails/app/controllers/posts_controller.rb
+++ homework/blog_on_rails/app/controllers/posts_controller.rb
@@ -1,9 +1,27 @@
 class PostsController < ApplicationController
-  def homepage
-    render :home
-    @a = "";
+  def home
   end
-  # def new
-  #   render :new
-  # end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+    if @post.valid?
+      @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @posts = Post.all.order("id DESC")
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
