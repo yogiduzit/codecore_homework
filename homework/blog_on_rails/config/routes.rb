9d506a4222608641e98d5d888b8442a399db6ab5
yogiduzit <yverma2@my.bcit.ca>
2019-07-10 18:17:42 -0700

    can add new posts

--- homework/blog_on_rails/config/routes.rb
+++ homework/blog_on_rails/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/", {to: "posts#homepage", as: "root"}
+  get "/", {to: "posts#home", as: "root"}
 
   resources :posts
 end
