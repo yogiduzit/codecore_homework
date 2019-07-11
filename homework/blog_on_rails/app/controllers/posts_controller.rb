b24b20cd3afbe317c07376efac20c36fb40e98b2
yogiduzit <yverma2@my.bcit.ca>
2019-07-10 19:06:22 -0700

    added logic for edit, show, delete

--- homework/blog_on_rails/app/controllers/posts_controller.rb
+++ homework/blog_on_rails/app/controllers/posts_controller.rb
@@ -20,8 +20,39 @@ class PostsController < ApplicationController
     @posts = Post.all.order("id DESC")
   end
 
+  def show
+    @post = find_post
+    # p time_ago_in_words @post["created_at"] =>  This is a helper so it can be only used in the views file
+  end
+
+  def edit
+    @post = find_post
+
+
+  end
+
+  def update
+    @post = find_post
+
+    if @post.update post_params
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    post = find_post
+    post.destroy
+    redirect_to posts_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    Post.find(params["id"])
   end
 end
