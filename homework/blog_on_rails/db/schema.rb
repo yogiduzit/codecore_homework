c04e38cbd275b8f7641d9519222641b2bbdce16e
yogiduzit <yverma2@my.bcit.ca>
2019-07-10 19:19:40 -0700

    Added the comments relation

--- homework/blog_on_rails/db/schema.rb
+++ homework/blog_on_rails/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_001638) do
+ActiveRecord::Schema.define(version: 2019_07_11_021451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "posts_id"
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
